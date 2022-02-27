2950b8d61603630d83c464ae571edd6569c82ec8
HongXin <hongxin@jd.com>
2022-02-27 16:23:21 +0800

    add example dir

---
+++ example/webpack.config.js
@@ -0,0 +1,29 @@
+import path from 'path'
+
+const url = new URL(import.meta.url)
+const __dirname = url.pathname
+
+export default {
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname, './src/entry1.js'),
+        second: path.resolve(__dirname, './src/entry2.js'),
+    },
+    context: process.cwd(),
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: '[name].js'
+    },
+    resolve:{
+        extensions: ['.js', '.ts']
+    },
+    module: {
+        rules: [{
+            test: /\.js/,
+            use: [
+                path.resolve(__dirname, './loaders/loader-1.js')
+            ]
+        }]
+    },
+    plugins: [new PluginA()]
+}
